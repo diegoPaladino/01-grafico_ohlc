ff3317c0c56ea8015c094f7e95b12f283ddb5c6b
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-24 21:03:15 -0300

    202009242103

--- seaborn_first_test.py
+++ seaborn_first_test.py
@@ -7,11 +7,20 @@
 
 # plt.show()
 
+#------------------------------------
 
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
 
-sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
+# plt.show()
 
-plt.show()
+#------------------------------------
+
+import seaborn as sns, numpy as np
+sns.set_theme(); np.random.seed(0)
+x = np.random.randn(100)
+ax = sns.distplot(x)
+
+#this last was failed!
